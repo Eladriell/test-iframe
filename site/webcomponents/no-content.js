039efdab86c917ca864a379aae2c5c8d299bbcf8
Jeremy BOURGEOIS <jeremy.bourgeois@amadeus.com>
2021-09-01 11:34:42 +0200

    foo

--- site/webcomponents/no-content.js
+++ site/webcomponents/no-content.js
@@ -2,6 +2,7 @@ const demoComponentNoContentTemplate = document.createElement('template');
 demoComponentNoContentTemplate.innerHTML = `
 <div>
   <span>Web component!</span>
+  <span>Current cart ID is: <span id="cart-id"></span></span>
 </div>
 `;
 
@@ -9,23 +10,31 @@ class DemoComponentNoContent extends HTMLElement {
 
     cartId;
 
+    contentNode;
+
     static get observedAttributes() {
         return ['cartId']
     }
 
     constructor() {
-        super()
-            .attachShadow({ mode: "open" })
-            .appendChild(demoComponentNoContentTemplate.content.cloneNode(true))
+        super();
+        const root = this.attachShadow({ mode: "open" });
+        this.appendChild(demoComponentNoContentTemplate.content.cloneNode(true));
+        this.contentNode = root.getElementById('cartId');
     }
 
     connectedCallback() {
         console.log('[DemoComponent] connected!');
+        this.render();
     }
 
     attributeChangedCallback(name, oldVal, newVal) {
         this[name] = newVal;
         console.log(`[DemoComponent] New value for attribute '${name}': ${oldVal} => ${newVal}`);
+    }
+
+    render() {
+        this.contentNode.innerText = this.cartId || 'None';
     }
 }
 
