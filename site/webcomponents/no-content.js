cf412e1c29400e3ac176b6a01b596681c08b38ae
Jeremy BOURGEOIS <jeremy.bourgeois@amadeus.com>
2021-09-01 10:47:26 +0200

    add web component

---
+++ site/webcomponents/no-content.js
@@ -0,0 +1,32 @@
+const demoComponentNoContentTemplate = document.createElement('template');
+demoComponentNoContentTemplate.innerHTML = `
+<div>
+  <span>Web component!</span>
+</div>
+`;
+
+class DemoComponentNoContent extends HTMLElement {
+
+    cartId;
+
+    static get observedAttributes() {
+        return ['cartId']
+    }
+
+    constructor() {
+        super()
+            .attachShadow({ mode: "open" })
+            .appendChild(demoComponentNoContentTemplate.content.cloneNode(true))
+    }
+
+    connectedCallback() {
+        console.log('[DemoComponent] connected!');
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        this[name] = newVal;
+        console.log(`[DemoComponent] New value for attribute '${name}': ${oldVal} => ${newVal}`);
+    }
+}
+
+window.customElements.define('demo-component', DemoComponent);
