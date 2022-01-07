cb5a83fc98591fb9a95183ccd180cb531b36c76c
Jeremy BOURGEOIS <jeremy.bourgeois@amadeus.com>
2022-01-07 09:32:26 +0100

    start using bridge

---
+++ site/scripts/script-not-using-bridge.js
@@ -0,0 +1,21 @@
+function interactWithScript() {
+    return 'You have interacted with a 3rd party script';
+}
+
+function getDisplayablePiece(cartId) {
+    return `<div>
+    <h2>Content from partner's script</h2>
+    <div>Cart ID is: ${cartId}</div>
+</div>`;
+}
+
+let cartId;
+
+function registerCartId(cartId) {
+    cartId = cartId;
+    console.log(`iFrame received cart ID: ${cartId}`);
+
+    setTimeout(() => {
+        window.dispatchEvent(new CustomEvent('triggerContent', { detail: getDisplayablePiece(cartId)}));
+    }, 3000)
+}
--- site/scripts/test-worker.js
+++
@@ -1,3 +0,0 @@
-function interactWithScript() {
-    return 'You have interacted with a 3rd party script';
-}
