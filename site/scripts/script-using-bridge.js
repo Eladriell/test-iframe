1187a9f8da224447fb74b6b11988f5403e6c9eb2
Jeremy BOURGEOIS <jeremy.bourgeois@amadeus.com>
2022-01-25 11:34:17 +0100

    add leave intent

--- site/scripts/script-using-bridge.js
+++ site/scripts/script-using-bridge.js
@@ -2,32 +2,46 @@ function interactWithScript() {
     return 'You have interacted with a 3rd party script';
 }
 
-function getDisplayablePiece(cartId) {
+function getDisplayablePiece(cart) {
     return `<div>
     <h3>Content from partner's script</h3>
     <div>Manipulating the Bridge</div>
-    <div>Cart ID is: ${cartId}</div>
+    <div>Cart ID is: ${cart.id}</div>
 </div>`;
 }
 
-let cartId;
-let email;
+let userCart;
+let userEmail;
 
 function registerCart(cart) {
-    if (cartId != cart.id) {
-        cartId = cart.id;
-        console.log(`[Bridge][iFrame] register new cart ID: ${cartId}`);
+    if (!userCart || userCart.id != cart.id) {
+        userCart = cart;
+        console.log(`[Bridge][iFrame] register new cart ID: ${userCart.id}`);
 
         setTimeout(() => {
-            BRIDGE.sendMessage({action: 'SHOW_TEASER', version: '1.0', data: getDisplayablePiece(cartId)});
+            BRIDGE.sendMessage({action: 'SHOW_TEASER', version: '1.0', data: getDisplayablePiece(userCart)});
         }, 3000);
     }
 
     const emailAddress = cart && cart.contacts && cart.contacts.find((contact) => contact.contactType === 'Email');
-    if (emailAddress && email != emailAddress.address) {
-        email = emailAddress.address;
-        console.log(`[Bridge][iFrame] cart now contains an email address: ${email}`);
+    if (emailAddress && userEmail != emailAddress.address) {
+        userEmail = emailAddress.address;
+        console.log(`[Bridge][iFrame] cart now contains an email address: ${userEmail}`);
         // do something with this new data
+    }
+}
+
+function onLeaveIntent() {
+    if (userCart && userEmail) {
+        const bounds = userCart.airOffers[0].offerItems[0].air.bounds;
+        const origin = bounds[0].originLocation;
+        const destination = bounds[0].destinationLocation;
+
+        const content = `<h2>Are your leaving us?</h2>
+<p>Don't want to finish your booking from ${origin.cityCode} (${origin.airportName}) to ${destination.cityCode} (${destination.airportName}) now?</p>
+<p>You will be reminded later by email at ${userEmail} so you can resume from where you left!</p>
+`;
+        BRIDGE.sendMessage({action: 'SHOW_TEASER', version: '1.0', data: content});
     }
 }
 
@@ -41,6 +55,9 @@ BRIDGE.register((message) => {
         case 'SHOPPING_CART':
             console.log('[Bridge][iFrame] received Cart: ', message.data);
             registerCart(message.data);
+            break;
+        case 'LEAVE_INTENT':
+            onLeaveIntent();
             break;
         default:
             console.warn('[Bridge][iFrame] received unsupported action ' + message.action);
